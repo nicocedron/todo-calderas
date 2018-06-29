20ba09024caafb01a99a3363fe03b60f63eb8059
Nicolas Cedron <josenicolascedron@gmail.com>
2018-06-28 21:38:56 -0300

    Convert S3 protocol to sym

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     url: ':s3_alias_url',
     path: '/:class/:attachment/:id_partition/:style/:filename',
     s3_host_alias: ENV.fetch('S3_HOST_NAME'),
-    s3_protocol: ENV.fetch('S3_PROTOCOL'),
+    s3_protocol: ENV.fetch('S3_PROTOCOL').to_sym,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
