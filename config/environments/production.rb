aba040fed52f9e6b3fce58405cdaaf30c090093f
Nicolas Cedron <josenicolascedron@gmail.com>
2018-06-30 15:47:25 -0300

    Add cache and gzip to production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
+
+  config.assets.gzip = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
